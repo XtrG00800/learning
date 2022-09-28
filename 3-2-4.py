47adb1a501c8e242eac8b5dbace3def31197383b
jeliasson <jeliasson@noreply.users.github.com>
2022-09-28 17:26:53 +0200

    I think I fixed the problem. Please let me know.

--- 3-2-4.py
+++ 3-2-4.py
@@ -23,48 +23,23 @@
 #about printing the final message.
 
 
-#Add your code here!
-
+# Teams
 team_1 = "Georgia Tech"
-team_1_score = 29
+team_1_score = 2
 team_2 = "Georgia"
-team_2_score = 28
+team_2_score = 2
 
+# Calculate score
 result = team_1_score - team_2_score
 
-print()
-geotechw = "Georgia Tech beat Georgia by"
-geow = "Georgia beat Georgia Tech by"
+# Make sure negative becomes positive
 result1 = abs(result)
 
-
-if team_1_score >= team_2_score and not team_1_score == team_2_score:
-    print(geotechw, result1)
-
-else:
-    print("")
-
-if team_2_score >= team_1_score and not team_1_score == team_2_score:
-    print (geow, result1)
-
-else:
-    print("")
+if team_1_score > team_2_score:
+    print(f"{team_1} beats {team_2} by {result1}")
     
-    
-    
-
-
-if team_1 == "Georgia Tech":
-    print("")
+elif team_2_score > team_1_score:
+    print(f"{team_2} beats {team_1} by {result1}")
 
 else:
-    print("")
-
-
-count = 5
-while count > 0:
-    print("")
-    count -= 1
-else:
-    print("")
-
+    print(f"{team_1} played {team_2} and it was a tie")
